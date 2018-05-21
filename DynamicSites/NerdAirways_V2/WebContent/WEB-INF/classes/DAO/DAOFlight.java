b46d59973d2f64abd6d88900bd58520636df508b
SouthDB <38219134+SouthDB@users.noreply.github.com>
2018-05-22 00:14:22 +0200

    FlightStatus Tabelle jetzt dynamisch

---
+++ DynamicSites/NerdAirways_V2/WebContent/WEB-INF/classes/DAO/DAOFlight.java
@@ -0,0 +1,21 @@
+package DAO;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.servlet.ServletContext;
+
+import java.math.BigDecimal;
+import java.util.*;
+import DAO.*;
+import model.*;
+import servlet.*;
+import myfunction.*;
+
+public class DAOFlight {
+	
+	public Collection<AwFlight> listAll(ServletContext sc) {
+		EntityManager em = (EntityManager)sc.getAttribute("em");
+		return em.createNamedQuery("AwFlight.findAll").getResultList();
+	}
+	
+}
