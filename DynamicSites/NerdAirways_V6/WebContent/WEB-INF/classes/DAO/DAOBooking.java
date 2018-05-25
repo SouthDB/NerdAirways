d34fd85923f8d57ce1ab07345f4964d93b4e344a
SouthDB <38219134+SouthDB@users.noreply.github.com>
2018-05-25 12:28:41 +0200

    Konto Bereich ist jetzt "fertig" (ohne Inofrmationen updaten)

---
+++ DynamicSites/NerdAirways_V6/WebContent/WEB-INF/classes/DAO/DAOBooking.java
@@ -0,0 +1,32 @@
+package DAO;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.servlet.ServletContext;
+
+import java.math.BigDecimal;
+import java.util.*;
+import DAO.*;
+import model.*;
+import servlet.*;
+import myfunction.*;
+
+public class DAOBooking {
+	
+	public Collection<AwBooking> listId(BigDecimal bookingnr, ServletContext sc){
+		EntityManager em = (EntityManager)sc.getAttribute("em");
+		
+		
+		Query query = em.createQuery("SELECT b from AwBooking b where b.bookingnr like '" + bookingnr + "%' ORDER BY b.id");
+		return(Collection<AwBooking>)query.getResultList();
+	}
+	
+	public Collection<AwBooking> listByCustId(BigDecimal custid, ServletContext sc){
+		
+		EntityManager em = (EntityManager)sc.getAttribute("em");
+		Query query = em.createQuery("SELECT b from AwBooking b where b.custid like '" + custid + "%' ORDER BY b.id");
+		return(Collection<AwBooking>)query.getResultList();
+		
+	}
+	
+}
